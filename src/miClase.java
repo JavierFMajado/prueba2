48a2221ffa6360be845aea9845872dbb6a15ffbc
Javier Majado <fjfernandez@viewnext.com>
2019-01-24 17:56:56 +0100

    resolucion conflicto

--- src/miClase.java
+++ src/miClase.java
@@ -2,10 +2,9 @@
 public class miClase {
 
 	public static void main(String[] args) {
+		// Hola javier el Main no funciona, un saludo desde Checoslovakia.
+	
 
-		
-		//Holaaa dos commit
 	}
 
-	
 }
