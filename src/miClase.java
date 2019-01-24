dadfa59baf8d382e709771df7ac1d7bce7284f49
Ismael M R <ismaelViewNext@gmail.com>
2019-01-24 17:39:17 +0100

    Commit 05

--- src/miClase.java
+++ src/miClase.java
@@ -2,7 +2,8 @@
 public class miClase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Hola javier el Main no funciona, un saludo desde Checoslovakia.
+	
 
 	}
 
